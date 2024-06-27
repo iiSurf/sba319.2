f033a9a9080744de59c88fb961fff539a90be673
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-06-26 22:45:44 -0700

    taking a break

--- routes/theaters.js
+++ routes/theaters.js
@@ -2,6 +2,18 @@ import express from 'express';
 import Theaters from '../models/theaters.js';
 const router = express.Router();
 
-
+router
+    .route('/')
+    .get(async (req, res) => {
+        try {
+            const theaters = await Theaters.find({});
+            res.status(200).json(theaters);
+        } catch (error) {
+            res.status(500).json({message: 'Server is not responding'});
+        }
+    })
+    .post(async (req, res) => {
+        
+    })
 
 export default router;
